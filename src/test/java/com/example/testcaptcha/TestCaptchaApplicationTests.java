0e0e281f4d373cd2ccbc363de6930c21db17c9e7
ledangtuanbk <ledangtuanbk@users.noreply.github.com>
2024-07-03 09:39:44 +0700

    Update TestCaptchaApplicationTests.java

--- src/test/java/com/example/testcaptcha/TestCaptchaApplicationTests.java
+++ src/test/java/com/example/testcaptcha/TestCaptchaApplicationTests.java
@@ -1,11 +1,14 @@
 package com.example.testcaptcha;
 
 import org.junit.jupiter.api.Test;
+import java.util.List;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TestCaptchaApplicationTests {
 
+    private int a = 1;
+    
     @Test
     void contextLoads() {
         System.out.println("hello world");
